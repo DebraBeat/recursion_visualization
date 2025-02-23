cf6b1ad788c82a3d78dcb93b666f68b0d5543271
Debra Ritter <zeeritter@gmail.com>
2025-02-23 00:03:45 -0500

    Began fibonacci visualization with rectangles.

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from manim import *
+import numpy as np
 
-# Default code included from quickstart
-class CreateCircle(Scene):
+# TODO incorporate hashmap into manim visualization
+hashmap = {0: 1, 1: 1}
+
+def fib(i : int) -> int:
+    if i == 0 or i == 1:
+        return 1
+
+    return fib(i - 1) + fib(i - 2)
+class CreateRectangle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
-        self.wait(5)
+        colors = [BLUE, GOLD, GREEN, PINK, TEAL]
+        color_index = 0
+        # TODO  - Incorporate this as a list instead of rewriting the rectangle
+        #       Or use transform (possibly) to just have a "current"
+        #       and "previous" rectangle
+        #       - Animate rectangle movement
+        for i in range(0, 10):
+            rect = Rectangle(width=np.sqrt(fib(i)), height=np.sqrt(fib(i)))
+            rect.set_fill(colors[color_index], opacity=0.5)
+            color_index = (color_index + 1) % len(colors)
 
-class CreateStack(Scene):
+            self.play(Create(rect))
+            self.wait(2)
+            rect.move_to(RIGHT)
